76bfb34a21677c92273909e035a08463353f2f8a
Vincent NORGUET <vincent.norguet@gmail.com>
2013-07-13 11:45:45 +0000

    Use index.html in place of static string

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
+// IO dependencies
+var util = require('util');
+var fs = require('fs');
+//var rest = require('restler');
+//var csv = require('csv');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buffer = new Buffer(fs.readFileSync('./index.html'));
+//	buffer.write(fs.readFileSync('./index.html'));
+//	fs.readFileSync('./index.html');
+	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
