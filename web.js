75c5c1bc7fbc3f225354fbb41da4511f064d3633
Vincent NORGUET <vincent.norguet@gmail.com>
2013-07-17 20:38:05 +0200

    HW-4 Add CSS headers

--- web.js
+++ web.js
@@ -3,19 +3,23 @@ var express = require('express');
 // IO dependencies
 var util = require('util');
 var fs = require('fs');
-//var rest = require('restler');
-//var csv = require('csv');
 
 var app = express.createServer(express.logger());
 
+console.log(app);
 app.get('/', function(request, response) {
-	var buffer = new Buffer(fs.readFileSync('./index.html'));
-//	buffer.write(fs.readFileSync('./index.html'));
-//	fs.readFileSync('./index.html');
-	response.send(buffer.toString('utf-8'));
+//  console.log(request['route']);
+  var buffer = new Buffer(fs.readFileSync('./index.html'));
+  response.send(buffer.toString('utf-8'));
+});
+app.get('/assets/:type/:filename', function(request, response) {
+  console.log('into assets' + request.params.type + ' ' + request.params.filename);
+  var buffer= new Buffer(fs.readFileSync('assets/' + request.params.type + "/" + request.params.filename));
+  response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
