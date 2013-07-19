da07eb35c2e8c22061ca1fed7609b82b80ceba54
Vincent NORGUET <vnorguet@heliostech.fr>
2013-07-19 20:24:02 +0200

    HW-4 Use external images

--- web.js
+++ web.js
@@ -6,20 +6,20 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-console.log(app);
 app.get('/', function(request, response) {
-//  console.log(request['route']);
-  var buffer = new Buffer(fs.readFileSync('./index.html'));
-  response.send(buffer.toString('utf-8'));
-});
-app.get('/assets/:type/:filename', function(request, response) {
-  console.log('into assets' + request.params.type + ' ' + request.params.filename);
-  var buffer= new Buffer(fs.readFileSync('assets/' + request.params.type + "/" + request.params.filename));
-  response.send(buffer.toString('utf-8'));
+	var buffer = new Buffer(fs.readFileSync('./index.html'));
+	response.send(buffer.toString('utf-8'));
 });
 
+/*
+app.get('/assets/:type/:filename', function(request, response) {
+	console.log('into assets' + request.params.type + ' ' + request.params.filename);
+	var buffer= new Buffer(fs.readFileSync('assets/' + request.params.type + "/" + request.params.filename));
+	response.send(buffer.toString('utf-8'));
+});
+*/
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
